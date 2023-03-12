0a9d13aa31e6e39a85470a13fd5127cf652288d7
heymimal <mimalreis@gmail.com>
2023-03-12 16:32:16 +0000

    changed placements and created enum for operations

--- src/tintolmarket/Tintolmarket.java
+++ src/tintolmarket/Tintolmarket.java
@@ -1,3 +1,5 @@
+package tintolmarket;
+
 import java.util.Scanner;
 
 import tintolmarket.client.Client;
---
+++ src/tintolmarket/domain/Operacao.java
@@ -0,0 +1,12 @@
+package tintolmarket.domain;
+
+public enum Operacao {
+	ADD,
+	VIEW,
+	BUY,
+	SELL,
+	WALLET,
+	CLASSIFY,
+	TALK,
+	READ
+}
