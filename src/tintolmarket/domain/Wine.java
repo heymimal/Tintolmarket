7c3c816403e54ebe319beaab2ab4d05c7cdb9282
heymimal <mimalreis@gmail.com>
2023-03-16 15:46:32 +0000

    wine constructor + getters/setters

--- src/tintolmarket/domain/Wine.java
+++ src/tintolmarket/domain/Wine.java
@@ -1,14 +1,29 @@
 package tintolmarket.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class Wine implements Serializable {
-	private String name;
-	//private Client[] clients;
-	// quantity value
-	//private double rating; 
-	//maybe serialize to file data, idk
-	public Wine(String winename) {
-		this.name = winename;
+	private static final long serialVersionUID = 1L;
+	private String wineName;
+	private double rating;
+	private List<Cliente> infoClientes;
+	private int totalAmount;
+	
+	public Wine(String wineName) {
+		this.setWineName(wineName);
+		this.infoClientes = new ArrayList<>();
+		this.rating = -1;
+		this.setTotalAmount(0);
+	}
+	private void setTotalAmount(int i) {
+		this.totalAmount = i;
+		
+	}
+	public void setWineName(String wineName) {
+		this.wineName = wineName;
 	}
 }
