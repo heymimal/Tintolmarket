2c938769a53af502d1fe3b98e5eb5e9a4911af85
heymimal <mimalreis@gmail.com>
2023-03-16 16:19:56 +0000

    more winehandler operations

--- src/tintolmarket/handlers/WineHandler.java
+++ src/tintolmarket/handlers/WineHandler.java
@@ -39,5 +39,42 @@ public class WineHandler {
 		//TO DO
 		return this.catwine.viewWine(winename);
 	}
+	public int buyWine(String winename, String seller, String user, int quantity) {
+		Wallet walletUser = getWalletUser(user);
+		if(walletUser.getClass() == Wallet.class) {
+			int wallet = walletUser.getWallet();
+			int change = this.catwine.buyWine(winename, seller, quantity, wallet);
+			if(change <= 0) {
+				return change;
+			}
+			System.out.println(change);
+			Wallet walletSeller = getWalletUser(seller);
+			if(walletSeller.getClass() == Wallet.class) {
+				int new_walletUser = wallet - change;
+				int new_walletSeller = walletSeller.getWallet() + change;
+				walletSeller.changeWallet(new_walletSeller);
+				walletUser.changeWallet(new_walletUser);
+				this.catwallet.changeWallet(walletUser);
+				this.catwallet.changeWallet(walletSeller);
+				return 1;
+			}
+			
+		}
+		return 0; // user non_existent -> impossible
+		
+		
+	}
+	
+	public int getWallerUser(String username) {
+		return this.catwallet.getWalletValue(username);
+	}
+	
+	private Wallet getWalletUser(String username) {
+		return this.catwallet.getWalletUser(username);
+	}
+	
+	public boolean classify(String winename,int rating) {
+		return this.catwine.rateWine(winename, rating);
+	}
 
 }
